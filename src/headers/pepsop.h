5df66c8e314dfaf0106f5f16f66cb8006cde3022
James_Allen <jamesza2@illinois.edu>
2020-10-13 19:54:52 -0500

    prints

--- src/headers/pepsop.h
+++ src/headers/pepsop.h
@@ -156,8 +156,12 @@ class Term{
 			for(auto ops_it = ops.begin(); ops_it != ops.end(); ops_it++){
 				itensor::ITensor T = ops_it->tensor(PEPS1.site_indices[*sites_it]);
 				auto [i,j,k] = PEPS1.position_of_site(*sites_it);
+				std::cerr << "Original tensor: " << std::endl;
+				PrintData(PEPS1._site_tensors[i][j][k]);
 				PEPS1._site_tensors[i][j][k] *= T;
 				PEPS1._site_tensors[i][j][k] *= itensor::delta(PEPS1.site_indices[*sites_it], itensor::prime(PEPS1.site_indices[*sites_it]));
+				std::cerr << "Tensor after " << to_string() << std::endl;
+				PrintData(PEPS1._site_tensors[i][j][k]);
 				sites_it++;
 			}
 			
