23493fc2db3623957b4118a0703414d25c6c2c7c
James_Allen <jamesza2@illinois.edu>
2020-11-30 18:50:25 -0600

    prints

--- src/headers/gradient_optimization.h
+++ src/headers/gradient_optimization.h
@@ -234,7 +234,7 @@ double update(MCKPEPS &psi,
 	}
 
 	std::cerr << std::setprecision(6) << std::defaultfloat;
-	std::cerr << "DeltaE norm: " << itensor::norm(DeltaE.at(site)) << ", Delta*E norm: " << itensor::norm(Delta.at(site)*E);
+	std::cerr << "DeltaE norm: " << itensor::norm(DeltaE.at(target_site)) << ", Delta*E norm: " << itensor::norm(Delta.at(target_site)*E);
 	for(int site = 0; site < Delta.size(); site++){
 		auto [i,j,k] = psi.position_of_site(site);
 		psi._site_tensors[i][j][k] -= update_size*r.rand()*grads[site];
